da04025f94faeed8625ec698d9a76288513406d6
Sijoon Choi <ahslxj1993@naver.com>
2022-07-11 17:56:49 +0900

    학습내역 업로드

--- validate3.js
+++ validate3.js
@@ -1,9 +1,49 @@
 $(document).ready(function() {
-	$('#myform').submit(function() {
+
+	//ID 중복검사를 위해 부분
+	var is_idcheck_click = false; // id중복검사 했는지 여부
+	var idcheck_value = ''; //id 중복검사시 값
+
+	
+	$('#myform > div > fieldset > div:nth-child(3) > input[type=button]:nth-child(2)').click(function() {
+		var input_id = $.trim($('#id').val());
+		if ($.trim($('#id').val()) == "") {//아이디 입력을 안했을때
+			alert("ID를 입력하세요");
+			$('#id').focus();
+			return false;
+		} else { //아이디를 입력했을때
+			//첫글자는 대문자이고 두번째부터는 대소문자, 숫자, _ 로 총 4개이상
+			pattern = /^[A-Z][a-zA-Z_0-9]{3,}$/;
+
+			if (pattern.test(input_id)) { //id 값이 정규 표현식을 만족하는지 체크합니다
+				idcheck_value = input_id;
+				is_idcheck_click = true;
+
+				var ref = "idcheck.html?id=" + $('#id').val();
+
+				//팝업창을 이용해 자료를 넘김
+				window.open(ref, "idcheck", "width=350, height=250")
+			} else {
+				alert("첫글자는 대문자이고 두번째부터는 대소문자,숫자,_로 총 4개 이상이어야 합니다.");
+				$('#id').val('').focus();
+				return false;
+			}
+		}
+	})//click value=ID중복검사 input
+	
+	
+	$('form').submit(function() {
 		//id 공백 유효성 검사
 		if ($.trim($('#id').val()) == '') {
 			alert('아이디를 입력하세요');
 			$('#id').focus();
+			return false;
+		}
+		
+		//아이디 중복검사 유효성 검사
+		var submit_id_value=$.trim($('#id').val());
+		if(!is_idcheck_click || submit_id_value != idcheck_value) {
+			alert("ID중복검사를 실행해주세요");
 			return false;
 		}
 
@@ -67,8 +107,8 @@ $(document).ready(function() {
 
 		//취미를 두개이상  체크하도록 하는 유효성 검사
 		var cnt = $('input:checkbox:checked').length;
-		
-		
+
+
 		/*var cnt = 0;
 		var hobbies = $('input[name=hobby]:checked');
 		for (var i = 0; i < hobbies.length; i++) {
@@ -87,14 +127,21 @@ $(document).ready(function() {
 			post1.focus();
 			return false;
 		}
+		
+		
 
 		//우편번호 숫자 유효성 검사
-		if (!$.isNumeric($('#post1').val())){
+		if (!$.isNumeric($('#post1').val())) {
 			alert('우편번호는 숫자만 입력 가능합니다.');
 			$('#post1').val('').focus();
 			return false;
 		}
 		
+		//우편번호 다섯자리 유효성 검사
+		if ($.trim($('#post1').val()).length!=5){
+			alert('우편번호는 다섯자리입니다');
+			return false;
+		}
 		
 		//주소 공백 유효성 검사
 		if ($('#address').val() == "") {
@@ -109,37 +156,10 @@ $(document).ready(function() {
 			intro.focus();
 			return false;
 		}
+
+
 	})//myform submit
 
-	//ID 중복검사 부분<input type=button value=ID중복검사 onClick="idcheck()">
-	$('#myform > div > fieldset > div:nth-child(3) > input[type=button]:nth-child(2)').click(function() {
-
-		if ($('#id').val().trim() == "") {//아이디 입력을 안했을때
-			alert("ID를 입력하세요");
-			$('#id').focus();
-			return false;
-		} else { //아이디를 입력했을때
-			//첫글자는 대문자이고 두번째부터는 대소문자, 숫자, _ 로 총 4개이상
-			//^ 시작
-			//$ 끝
-			pattern = /^[A-Z][a-zA-Z_0-9]{3,}$/;
-
-			if (pattern.test($('#id').val())) { //id 값이 정규 표현식을 만족하는지 체크합니다
-				//만족하면 true, 만족하지 않으면 false를 반환합니다
-
-				//사용자가 입력한 방식을 get 방식으로 넘깁니다
-				var ref = "idcheck.html?id=" + id.value;
-
-				//팝업창을 이용해 자료를 넘김
-				window.open(ref, "", "width=300, height=250")
-			} else {
-				alert("첫글자는 대문자이고 두번째부터는 대소문자,숫자,_로 총 4개 이상이어야 합니다.");
-				$('#id').val('');
-				$('#id').focus();
-				return false;
-			}
-		}
-	})//click value=ID중복검사 input
 
 	//앞자리 정규식에 맞추면 뒷자리로 포커스 이동
 	//<input  type=text size=6 maxLength=6 name=jumin1 id=jumin1  onKeyup="move()" placeholder='주민번호 앞자리'>
@@ -170,8 +190,8 @@ $(document).ready(function() {
 				//주민번호 뒷자리에 따라 남자여자 성별 라디오 버튼 자동선택
 				var c = Number($(this).val().substring(0, 1));
 				var index = (c - 1) % 2; //c=1 or c=3 -> index=0 -> 1 -> "gender1"
-										 //c=2 or c=4 -> index=1 ->2 -> "gender2"
-				$('input[type=radio]').eq(index).prop('checked',true);
+				//c=2 or c=4 -> index=1 ->2 -> "gender2"
+				$('input[type=radio]').eq(index).prop('checked', true);
 			} else {
 				alert('숫자 또는 형식에 맞게 입력하세요');
 				$(this).val('');
@@ -182,19 +202,19 @@ $(document).ready(function() {
 
 	//도메인 셀렉터
 	//select 태그에서 선택한 도메인 설정
-	$('#sel').on('change', function () {
+	$('#sel').on('change', function() {
 
 		if ($(this).val() == "") {//직접입력 선택된 경우
-			$('#domain').prop('readonly',false);
+			$('#domain').prop('readonly', false);
 			$('#domain').val('').focus();
 		} else { //option 중에서 선택한 경우
-			$('#domain').prop('readonly',true); //수정불가
+			$('#domain').prop('readonly', true); //수정불가
 			$('#domain').val($('#sel').val());
 		}
 	})//onchange sel
-	
+
 	//<input type=button value="우편검색" onclick="post()">
-	$('input[value="우편검색"]').click(function () {
-		window.open("post.html","post","width=400, height=500, scrollbars=yes")
+	$('input[value="우편검색"]').click(function() {
+		window.open("post.html", "post", "width=400, height=500, scrollbars=yes")
 	})//onclick input value=우편검색
 })//ready
